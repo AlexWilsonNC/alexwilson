d3bc49ceee75283112190e544a266bb114aea718
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-01-10 12:55:12 -0500

    resume

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let darkMode = localStorage.getItem('darkMode');
-let spaceMode = localStorage.getItem('spaceMode');
+// let spaceMode = localStorage.getItem('spaceMode');
 
 const darkButton = document.getElementById('theme-btn');
-const spaceButton = document.getElementById('theme-btn-space')
-const alienShip = document.getElementById('spaceship');
+// const spaceButton = document.getElementById('theme-btn-space')
+// const alienShip = document.getElementById('spaceship');
 
 // DARK
 //////////////////////////////////////////////////////////////
@@ -36,33 +36,31 @@ darkButton.addEventListener('click', () => {
 
 // SPACE
 ////////////////////////////////////////////////////////////
-const enableSpaceMode = () => {
-  document.body.classList.add('space');
-  document.body.classList.remove('dark');
-  document.getElementById('theme-btn-space').innerText = 'Main Theme';
-  document.getElementById('tryme').style.display = 'none';
-  // document.body.classList.remove('hide-ship');
-  document.getElementById('spaceship').style.left = '3000px';
-  document.getElementById('spaceship').style.top = '-200px';
-  localStorage.setItem('spaceMode', 'enabled');
-};
+// const enableSpaceMode = () => {
+//   document.body.classList.add('space');
+//   document.body.classList.remove('dark');
+//   document.getElementById('theme-btn-space').innerText = 'Main Theme';
+//   document.getElementById('tryme').style.display = 'none';
+//   document.getElementById('spaceship').style.left = '3000px';
+//   document.getElementById('spaceship').style.top = '-200px';
+//   localStorage.setItem('spaceMode', 'enabled');
+// };
 
-const disableSpaceMode = () => {
-  document.body.classList.remove('space');
-  document.getElementById('theme-btn-space').innerText = 'Outer Space';
-  localStorage.setItem('spaceMode', null);
-};
+// const disableSpaceMode = () => {
+//   document.body.classList.remove('space');
+//   document.getElementById('theme-btn-space').innerText = 'Outer Space';
+//   localStorage.setItem('spaceMode', null);
+// };
 
-if(spaceMode === 'enabled') {
-  enableSpaceMode();
-}
+// if(spaceMode === 'enabled') {
+//   enableSpaceMode();
+// }
 
-spaceButton.addEventListener('click', () => {
-  spaceMode = localStorage.getItem('spaceMode');
-  if (spaceMode !== 'enabled') {
-    enableSpaceMode();
-    // moveShip();
-  } else {
-    disableSpaceMode();
-  }
-});
+// spaceButton.addEventListener('click', () => {
+//   spaceMode = localStorage.getItem('spaceMode');
+//   if (spaceMode !== 'enabled') {
+//     enableSpaceMode();
+//   } else {
+//     disableSpaceMode();
+//   }
+// });
